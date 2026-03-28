7ddfb21b39d5cab077659005e9f58fe6a114ffec
Sujan Magar <sujanmagar05825@gmail.com>
2026-03-28 17:16:47 +0545

    add

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,8 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
+    origin: ['http://localhost:5173',
+       'https://book.sujan.com.np',],
     credentials: true
 }))
 
