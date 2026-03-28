f50c907967d99745636a1e75e3e7868c0600517f
Sujan Magar <sujanmagar05825@gmail.com>
2026-03-28 17:42:02 +0545

    add

--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -7,7 +7,7 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_Auth_Domain,
+  authDomain: "book-store-mern-app-1c8f6.firebaseapp.com",
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDERID,
